80e670d89bb48942734ca0da7dd2b6ab8d85d1cf
mustafaesamm <79550524+mustafaesamm@users.noreply.github.com>
2021-05-16 00:23:56 +0300

    Update apiform.php

--- apiform.php
+++ apiform.php
@@ -1,5 +1,6 @@
 <?php
- header('Content-Type:application/json');
+header("Access-Control-Allow-Origin: *");
+header('Content-Type:application/json');
 $dsn="mysql:host=localhost;dbname=the_users";  
 $user="root";
 $pass="";
