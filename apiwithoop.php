5872a8c785bd28a7603166c598742e251b40100a
mustafaesamm <79550524+mustafaesamm@users.noreply.github.com>
2021-05-15 23:31:47 +0300

    Update apiwithoop.php

--- apiwithoop.php
+++ apiwithoop.php
@@ -3,8 +3,8 @@
  header('Content-Type:application/json');
 class connect extends PDO {
     public function __construct(){
-        
-    parent:: __construct("mysql:host=localhost;dbname=id15516854_the_users",'id15516854_mustafa','o0dtK{Ox>9O031ex',
+//         put here you database 
+    parent:: __construct("mysql:host=localhost;dbname= name of database",'theusername','pass',
     array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
     $this->setAttribute(PDO::ATTR_ERRMODE , PDO::ERRMODE_EXCEPTION);
     $this->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
@@ -15,16 +15,14 @@ class api {
     function select(){
         $db= new connect;
         $users=array();
-        $data=$db->prepare('SELECT * From participants');
-        $data->execute();
+        $data=$db->prepare('SELECT * From example');
+        $data->execute()
         while($uotput = $data->fetch(PDO::FETCH_ASSOC)){
             // $users[$uotput['name']]=array (
             $users[]=array (
             'name'=>$uotput['name'],
             'email'=>$uotput['email'],
             'phone'=>$uotput['phone'],
-            'type'=>$uotput['type'],
-            'course'=>$uotput['course']
             );
         }
       return  json_encode($users);
